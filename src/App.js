4492574a5c3472c1fde343f1ccd6ec8430170426
iamnishanth <nishanthvenk68@gmail.com>
2021-05-13 22:47:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Intro from "./components/Intro";
+import Container from "./components/Container";
+import Form from "./components/Form";
+import Mentor from "./components/Mentor";
+import Student from "./components/Student";
+import { fetchAll } from "./utils/netwokHandler";
+
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({
+    mentors: [],
+    students: [],
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      let res = await fetchAll();
+      console.log(res);
+      setData(res);
+    };
+    fetchData();
+  }, []);
+
+  const refreshData = async () => {
+    let res = await fetchAll();
+    console.log(res);
+    setData(res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => (
+              <div>
+                <Intro />
+                <Container data={data} />
+              </div>
+            )}
+          />
+          <Route
+            exact
+            path="/add-mentor"
+            component={() => <Form title="Mentor" refreshData={refreshData} />}
+          />
+          <Route
+            exact
+            path="/add-student"
+            component={() => <Form title="Student" refreshData={refreshData} />}
+          />
+          <Route exact path="/mentor/:id" component={Mentor} />
+          <Route exact path="/student/:id" component={Student} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
